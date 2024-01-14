dda8e81c23cfab84d5b09450c38ed45f2241c222
dryadsoft <dryadsoft@gmail.com>
2024-01-14 11:29:32 +0900

    add Flex properties

--- packages/storybook/src/components/atoms/flex/Flex.stories.tsx
+++ packages/storybook/src/components/atoms/flex/Flex.stories.tsx
@@ -1,16 +1,21 @@
 import { Meta, StoryObj } from "@storybook/react";
 import { Flex } from "./Flex";
+import { Box } from "../boxs/Box";
 
 const meta = {
   title: "atoms/Flex",
   component: Flex,
   parameters: {
     layout: "full",
-    controls: { exclude: ["children", "className"] },
+    controls: { exclude: ["children", "className"], sort: "alpha" },
   },
   tags: ["autodocs"],
   argTypes: {
     justifyContent: { control: "radio" },
+    alignItems: { control: "radio" },
+    rounded: { control: "radio" },
+    bgColor: { control: "radio" },
+    shadow: { control: "radio" },
   },
 } satisfies Meta<typeof Flex>;
 
@@ -20,12 +25,15 @@ type Story = StoryObj<typeof meta>;
 export const DefaultFlex: Story = {
   args: {
     justifyContent: "start",
+    alignItems: "center",
+    rounded: "none",
+    shadow: "none",
+    bgColor: "gray",
     children: (
       <>
-        <div className="w-10 h-10 bg-red-400"></div>
-        <div className="w-10 h-10 bg-red-400"></div>
-        <div className="w-10 h-10 bg-red-400"></div>
-        <div className="w-10 h-10 bg-red-400"></div>
+        <Box>test</Box>
+        <Box>test</Box>
+        <Box>test</Box>
       </>
     ),
   },
--- packages/storybook/src/components/atoms/flex/Flex.tsx
+++ packages/storybook/src/components/atoms/flex/Flex.tsx
@@ -1,21 +1,46 @@
-import { JustifyContent } from "../../../constants";
+import {
+  AlignItems,
+  FlexBgColor,
+  JustifyContent,
+  Position,
+  Rounded,
+  Shadow,
+} from "../../../constants";
 import { cls } from "../../../utils";
 
 interface FlexProps {
   justifyContent?: keyof typeof JustifyContent;
+  alignItems?: keyof typeof AlignItems;
+  bgColor?: keyof typeof FlexBgColor;
+  shadow?: keyof typeof Shadow;
+  rounded?: keyof typeof Rounded;
+  position?: keyof typeof Position;
+  className?: string;
   children?: React.ReactNode;
   [key: string]: unknown;
 }
 export const Flex = ({
+  bgColor = "transparent",
+  shadow = "none",
+  rounded = "none",
   justifyContent = "center",
+  alignItems = "center",
+  position = "static",
   children,
+  className,
   ...rest
 }: FlexProps) => {
   return (
     <div
       className={cls(
-        "flex items-center justify- gap-2 px-2 py-1 bg-gray-300 rounded-sm",
+        "flex gap-2 px-2 py-1",
         JustifyContent[justifyContent],
+        Shadow[shadow],
+        Rounded[rounded],
+        FlexBgColor[bgColor],
+        AlignItems[alignItems],
+        Position[position],
+        className!,
       )}
       {...rest}
     >
